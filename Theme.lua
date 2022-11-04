cbb89b44505621326d505a4aa5563b2e7de04ca6
Alzerys <117453422+AlzerysX@users.noreply.github.com>
2022-11-04 20:18:07 +0700

    Update Theme.lua

--- Theme.lua
+++ Theme.lua
@@ -1,11 +1,11 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-ThemeManager.Folder = "Delta-Hub-Settings"
+ThemeManager.Folder = "LinoriaLibSettings"
 ThemeManager.Library = nil
 ThemeManager.BuiltInThemes = {
-    ['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0055ff","BackgroundColor":"141414","OutlineColor":"323232"}') },
+    ['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"00ffdc","BackgroundColor":"141414","OutlineColor":"323232"}') },
     ['Green'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"141414","AccentColor":"00ff8b","BackgroundColor":"1c1c1c","OutlineColor":"3c3c3c"}') },
-    ['Jester'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
+    ['Night'] 	= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
 }
 
 function ThemeManager:ApplyTheme(theme)
